85365c3362a8bcd494f8062ba36ff983b211d3ea
Eric Schumann <eric.schumann82@gmail.com>
2026-09-02 10:49:05 -0500

    another vite.config.js fix

--- vite.config.js
+++ vite.config.js
@@ -24,9 +24,6 @@ export default defineConfig({
         sourcemap: true,
         rollupOptions: {
             output: {
-                manualChunks: {
-                    kaplay: ["kaplay"],
-                },
             },
         },
     },
